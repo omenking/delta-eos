345b78eb87187799067780fc29a88adad0aa8ce6
Andrew Brown <andrew@monsterboxpro.com>
2022-10-19 03:13:10 +0000

    docs

--- inventory_overlay.rb
+++ inventory_overlay.rb
@@ -5,6 +5,10 @@ include Curses
 class InventoryOverlay
   def self.draw mode
     return unless mode == :inventory_overlay
+
+    # lines is a curses function for the number
+    # of lines visible on the terminal
+    # https://tldp.org/LDP/lpg/node97.html
     lines.times.each do |row|
       if row < 3 || row > lines-4
         line = cols.times.map{|t|'╲'}.join('')
@@ -23,8 +27,11 @@ class InventoryOverlay
             ' '
           end
         end.join('')
+        # Draw the background and frame
         Game.str row, 0, line
       end
+
+      # Draw the heading 
       Game.str 5, 11, 'Inventory'
       line = 'Exit [X]'
       Game.str 5, cols-9-line.length, line
